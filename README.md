e36e8f4748ba525f1278c1f5ce16cf243ab005ad
Assassinss <931289622@qq.com>
2016-12-05 22:24:01 +0800

    Optimized movie detail ui

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A simple, unofficial android client of Eyepetizer(开眼视频).
 
 ###Screenshot
 <img src="screenshot/Screenshot_20161126-143303.png" width="216" height="384">
-<img src="screenshot/Screenshot_20161126-143311.png" width="216" height="384">
+<img src="screenshot/Screenshot_20161205-222100.png" width="216" height="384">
 <img src="screenshot/Screenshot_20161130-205250.png" width="216" height="384">
 <img src="screenshot/Screenshot_20161130-205151.png" width="216" height="384">
 <img src="screenshot/Screenshot_20161126-143729.png" width="216" height="384">
--- app/src/main/java/me/zsj/interessant/MovieDetailActivity.java
+++ app/src/main/java/me/zsj/interessant/MovieDetailActivity.java
@@ -3,6 +3,7 @@ package me.zsj.interessant;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -25,9 +26,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import me.zsj.interessant.api.ReplayApi;
+import me.zsj.interessant.model.ItemList;
 import me.zsj.interessant.model.Replies;
 import me.zsj.interessant.model.ReplyList;
-import me.zsj.interessant.model.ItemList;
 import me.zsj.interessant.utils.CircleTransform;
 import me.zsj.interessant.utils.TimeUtils;
 import me.zsj.interessant.widget.FabToggle;
@@ -61,6 +62,11 @@ public class MovieDetailActivity extends RxAppCompatActivity implements View.OnC
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
         setContentView(R.layout.movie_detail_activity);
         RecyclerView replies = (RecyclerView) findViewById(R.id.recycler_replies);
         final ParallaxScrimageView backdrop = (ParallaxScrimageView) findViewById(R.id.backdrop);
