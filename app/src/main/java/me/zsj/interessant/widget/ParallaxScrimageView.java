aecccf20cdbfb57bff7ab9b196edd1b104f4b6a5
Assassinss <931289622@qq.com>
2016-12-12 16:52:40 +0800

    Dynamically sets the color of ParallaxScrimageView

--- app/src/main/java/me/zsj/interessant/widget/ParallaxScrimageView.java
+++ app/src/main/java/me/zsj/interessant/widget/ParallaxScrimageView.java
@@ -5,7 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.annotation.FloatRange;
 import android.util.AttributeSet;
+import android.view.ViewOutlineProvider;
 import android.widget.ImageView;
 
 import me.zsj.interessant.utils.ColorUtils;
@@ -17,6 +19,10 @@ public class ParallaxScrimageView extends ImageView {
     private int imageOffset;
     private int minOffset;
     private Rect clipBounds = new Rect();
+
+    private float scrimAlpha = 0f;
+    private float maxScrimAlpha = 1f;
+    private int scrimColor = Color.TRANSPARENT;
 
 
     public ParallaxScrimageView(Context context) {
@@ -34,6 +40,8 @@ public class ParallaxScrimageView extends ImageView {
         float scrimAlpha = 0f;
         int scrimColor = Color.TRANSPARENT;
         scrimPaint.setColor(ColorUtils.modifyAlpha(scrimColor, scrimAlpha));
+
+        setOutlineProvider(ViewOutlineProvider.BOUNDS);
     }
 
     public void setOffset(int offset) {
@@ -44,8 +52,24 @@ public class ParallaxScrimageView extends ImageView {
             imageOffset = (int) (offset * parallaxFactor);
             clipBounds.set(0, -offset, getWidth(), getHeight());
             setClipBounds(clipBounds);
+            setScrimAlpha(Math.min(
+                    ((float) -offset / getMinimumHeight()) * maxScrimAlpha, maxScrimAlpha));
             postInvalidateOnAnimation();
         }
+        setPinned();
+    }
+
+    public void setScrimAlpha(@FloatRange(from = 0f, to = 1f) float alpha) {
+        if (scrimAlpha != alpha) {
+            scrimAlpha = alpha;
+            scrimPaint.setColor(ColorUtils.modifyAlpha(scrimColor, scrimAlpha));
+            postInvalidateOnAnimation();
+        }
+    }
+
+    private void setPinned() {
+        float elevation = (float) imageOffset / (float) getMinimumHeight();
+        setElevation(elevation * 20);
     }
 
     @Override
