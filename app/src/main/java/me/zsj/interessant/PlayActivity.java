7e0caf685fe3338eb760315e763642d1dd078d0f
Assassinss <931289622@qq.com>
2016-12-22 21:36:53 +0800

    Adjust ui detail

--- app/src/main/java/me/zsj/interessant/PlayActivity.java
+++ app/src/main/java/me/zsj/interessant/PlayActivity.java
@@ -5,7 +5,6 @@ import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
